fa11f7ce2d2ab52da691b438ec1b0b2516b69c3b
Pellux Network <69098091+pellux-network@users.noreply.github.com>
2025-07-08 22:43:29 -0600

    Readded cargo counting
    Fixed blank cargo page bug

--- edreader/modulesInfo.go
+++ edreader/modulesInfo.go
@@ -34,33 +34,32 @@ func handleModulesInfoFile(file string) {
 }
 
 func ModulesInfoCargoCapacity() int {
-	// if currentCargoCapacity > 0 {
-	// 	return currentCargoCapacity
-	// }
-	// cargoCapacity := 0
+	if currentCargoCapacity > 0 {
+		return currentCargoCapacity
+	}
+	cargoCapacity := 0
 
-	// for _, line := range currentModules.Modules {
-	// 	switch line.Item {
-	// 	case "int_cargorack_size1_class1":
-	// 		cargoCapacity += 2
-	// 	case "int_cargorack_size2_class1":
-	// 		cargoCapacity += 4
-	// 	case "int_cargorack_size3_class1":
-	// 		cargoCapacity += 8
-	// 	case "int_cargorack_size4_class1":
-	// 		cargoCapacity += 16
-	// 	case "int_cargorack_size5_class1":
-	// 		cargoCapacity += 32
-	// 	case "int_cargorack_size6_class1":
-	// 		cargoCapacity += 64
-	// 	case "int_cargorack_size7_class1":
-	// 		cargoCapacity += 128
-	// 	case "int_cargorack_size8_class1":
-	// 		cargoCapacity += 256
-	// 	}
-	// }
+	for _, line := range currentModules.Modules {
+		switch line.Item {
+		case "int_cargorack_size1_class1":
+			cargoCapacity += 2
+		case "int_cargorack_size2_class1":
+			cargoCapacity += 4
+		case "int_cargorack_size3_class1":
+			cargoCapacity += 8
+		case "int_cargorack_size4_class1":
+			cargoCapacity += 16
+		case "int_cargorack_size5_class1":
+			cargoCapacity += 32
+		case "int_cargorack_size6_class1":
+			cargoCapacity += 64
+		case "int_cargorack_size7_class1":
+			cargoCapacity += 128
+		case "int_cargorack_size8_class1":
+			cargoCapacity += 256
+		}
+	}
 
-	// return cargoCapacity
+	return cargoCapacity
 
-	return currentCargoCapacity
 }
